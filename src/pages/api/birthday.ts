85d66757b447965595f13c0ebb409f3ee2caa169
loic <loic.aviez91@gmail.com>
2023-06-08 11:41:49 +0200

    Modifications

--- src/pages/api/birthday.ts
+++ src/pages/api/birthday.ts
@@ -3,8 +3,6 @@ import {NextApiRequest, NextApiResponse} from "next";
 
 const prisma = new PrismaClient()
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
-
     if (req.method === 'GET') {
         const allStudents = await prisma.student.findMany()
         const today = new Date()
