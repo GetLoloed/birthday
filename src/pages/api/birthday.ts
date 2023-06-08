8ba777395c0f106d91180af226ce2b5b70b0d7fd
leotran671 <leo.tran@my-digital-school.org>
2023-06-08 11:45:21 +0200

    first commit of add method to send mail

--- src/pages/api/birthday.ts
+++ src/pages/api/birthday.ts
@@ -1,27 +1,52 @@
-import {PrismaClient} from '@prisma/client'
-import {NextApiRequest, NextApiResponse} from "next";
+import { PrismaClient } from '@prisma/client';
+import nodemailer from 'nodemailer';
+import cron from 'node-cron';
 
-const prisma = new PrismaClient()
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+const prisma = new PrismaClient();
 
+const sendBirthdayEmails = async () => {
+    const allStudents = await prisma.student.findMany();
+    const today = new Date();
 
-    if (req.method === 'GET') {
-        const allStudents = await prisma.student.findMany()
-        const today = new Date()
+    const studentsWithBirthdayToday = allStudents.filter(student => {
+        const birthday = new Date(student.birthday);
+        return birthday.getDate() === today.getDate() && birthday.getMonth() === today.getMonth();
+    });
 
-        const studentsWithBirthdayToday = allStudents.filter(student => {
-            const birthday = new Date(student.birthday)
-            return birthday.getDate() === today.getDate() && birthday.getMonth() === today.getMonth()
-        })
+    const transporter = nodemailer.createTransport({
+        port: 465,
+        host: 'node177-eu.n0c.com',
+        secure: true,
+        auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS,
+        },
+    });
 
-        // Map over the array to only keep the firstname and lastname properties
-        const result = studentsWithBirthdayToday.map(student => ({
-            firstname: student.firstname,
-            lastname: student.lastname,
-        }))
-
-        res.json(result)
-    } else {
-        res.status(405).json({message: 'Method not allowed'})
+    for (const student of studentsWithBirthdayToday) {
+        await transporter.sendMail({
+            from: `"Mon école" <${process.env.MAIL_USER}>`, // Expéditeur
+            to: student.email, // Destinataire
+            subject: 'Joyeux anniversaire!', // Sujet
+            text: `Joyeux anniversaire, ${student.firstname} ${student.lastname}!`, // Corps du courrier électronique
+            html: `<p>Joyeux anniversaire, ${student.firstname} ${student.lastname}!</p>`, // Corps du courrier électronique en HTML
+        });
     }
+
+    console.log('Birthday emails sent!');
+}
+
+// Schedule task to run at 8 AM every day
+cron.schedule('0 8 * * *', sendBirthdayEmails);
+
+
+// Map over the array to only keep the firstname and lastname properties
+const result = studentsWithBirthdayToday.map(student => ({
+    firstname: student.firstname,
+    lastname: student.lastname,
+}));
+res.json(result);
+    } else {
+    res.status(405).json({ message: 'Method not allowed' });
+}
 }
