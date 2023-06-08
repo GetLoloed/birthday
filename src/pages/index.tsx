897b5fe9b3d63b164e57d6001b819c10b07aef50
loic <loic.aviez91@gmail.com>
2023-06-08 11:43:18 +0200

    Ajout des background colors pour chaque components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,10 @@ import {Navbar} from '@/components/Navbar/Navbar';
 import hbround from '@/public/assets/svg/circle.svg';
 import Image from 'next/image';
 import {ImageContainer} from '@/components/ImageContainer/ImageContainer';
+import star from '@/public/assets/svg/star.svg';
 import {Name} from '@/components/Name/Name';
 import {GetServerSideProps} from 'next';
+import {BirthdayProvider} from "@/contexts/BirthdayProvider";
 
 interface Student {
     firstname: string;
@@ -16,36 +18,89 @@ interface HomeProps {
     studentsWithBirthdayToday: Student[];
 }
 
-const Home = ({studentsWithBirthdayToday}: HomeProps) => {
+const backgrounds = ['var(--pink)', 'var(--blue)', 'var(--green)', 'var(--yellow)'];
+
+const Home = ({ studentsWithBirthdayToday }: HomeProps) => {
     const [backgroundIndex, setBackgroundIndex] = useState(0);
-    const backgrounds = ['var(--pink)', 'var(--blue)', 'var(--green)', 'var(--yellow)'];
+    const [secondCount, setSecondCount] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
-        // Mettre à jour le fond toutes les 5 secondes
-        const interval = setInterval(() => {
+        const intervalId = setInterval(() => {
+            setSecondCount((prev) => (prev === 50 ? 0 : prev + 1));
+        }, 100);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    useEffect(() => {
+        if (secondCount === 0) {
             const randomIndex = Math.floor(Math.random() * backgrounds.length);
             setBackgroundIndex(randomIndex);
-        }, 5000);
-
-        return () => {
-            // Nettoyer l'intervalle lorsque le composant est démonté
-            clearInterval(interval);
-        };
-    }, []); // Dépendance vide pour s'assurer que l'effet ne se déclenche qu'une seule fois
+            setCurrentIndex((prev) => (prev + 1) % studentsWithBirthdayToday.length);
+        }
+    }, [secondCount, studentsWithBirthdayToday]);
 
     return (
-        <>
-            <Navbar/>
+        <BirthdayProvider>
+            <Navbar />
             <main className={styles.main}>
-                <div className={styles.left} style={{backgroundColor: backgrounds[backgroundIndex]}}>
-                    <Image src={hbround} alt={''}/>
-                    <Name studentsWithBirthdayToday={studentsWithBirthdayToday}/>
+                <div
+                    className={styles.left}
+                    style={{ backgroundColor: backgrounds[backgroundIndex] }}
+                >
+                    <div className={styles.circle}>
+                        <Image src={hbround} alt={''} />
+                    </div>
+                    <div className={styles.circlebg}></div>
+                    <div
+                        className={styles.circlebgRandom}
+                        style={{ backgroundColor: backgrounds[backgroundIndex] }}
+                    ></div>
+                    <Image
+                        className={styles.star1}
+                        src={star}
+                        alt={''}
+                        width={79}
+                        height={79}
+                    />
+                    <Image
+                        className={styles.star2}
+                        src={star}
+                        alt={''}
+                        width={79}
+                        height={79}
+                    />
+                    <Image
+                        className={styles.star3}
+                        src={star}
+                        alt={''}
+                        width={79}
+                        height={79}
+                    />
+                    <Name student={studentsWithBirthdayToday[currentIndex]} />
                 </div>
                 <div className={styles.right}>
-                    <ImageContainer/>
+                    <ImageContainer character={currentIndex} background={backgrounds[backgroundIndex]} />
+
+                    <div className={styles.progressContainer}>
+                        <div className={styles.number}>
+                            {Math.floor(secondCount / 10).toString().padStart(2, '0')}
+                        </div>
+                        <div className={styles.progressBar}>
+                            <div
+                                className={styles.progress}
+                                style={{
+                                    width: `${(secondCount / 50) * 100}%`,
+                                    backgroundColor: backgrounds[backgroundIndex],
+                                }}
+                            />
+                        </div>
+                        <div className={styles.number}>05</div>
+                    </div>
                 </div>
             </main>
-        </>
+        </BirthdayProvider>
     );
 };
 
