3eac6e62bfc15e437a912f68bbc96b5bbb743db0
loic <loic.aviez91@gmail.com>
2023-06-08 11:42:06 +0200

    Ajout du changement de couleur synchronysé avec le reste

--- src/components/ImageContainer/ImageContainer.tsx
+++ src/components/ImageContainer/ImageContainer.tsx
@@ -1,30 +1,23 @@
-import { useState, useEffect } from 'react';
 import char1 from '@/public/assets/svg/character_1.svg';
 import char2 from '@/public/assets/svg/character_2.svg';
 import char3 from '@/public/assets/svg/character_3.svg';
 import char4 from '@/public/assets/svg/character_4.svg';
 import Image from 'next/image';
-import styles from './ImageContainer.module.scss'
-export function ImageContainer() {
-    const [currentImage, setCurrentImage] = useState(char1);
+import styles from './ImageContainer.module.scss';
+
+interface ImageContainerProps {
+    character: number;
+    background: string;
+}
+
+export function ImageContainer({ character, background }: ImageContainerProps) {
     const images = [char1, char2, char3, char4]; // Liste des images possibles
-
-    useEffect(() => {
-        // Changer l'image toutes les 5 secondes
-        const interval = setInterval(() => {
-            const randomIndex = Math.floor(Math.random() * images.length);
-            setCurrentImage(images[randomIndex]);
-        }, 5000);
-
-        return () => {
-            // Nettoyer l'intervalle lorsque le composant est démonté
-            clearInterval(interval);
-        };
-    }, []); // Dépendance vide pour s'assurer que l'effet ne se déclenche qu'une seule fois
+    const currentImage = images[character % images.length];
 
     return (
-        <div className={styles.container}>
+        <div className={styles.container} >
             <Image src={currentImage} alt={''} />
+            <div className={styles.background} style={{ backgroundColor: background }}></div>
         </div>
     );
 }
